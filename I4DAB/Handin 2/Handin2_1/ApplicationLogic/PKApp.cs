bf146de9ed94e28e804dee9fe68e534f6ee23205
DannyTraan <201610981@post.au.dk>
2018-11-01 18:42:28 +0100

    Handin 3 DAB

---
+++ I4DAB/Handin 2/Handin2_1/ApplicationLogic/PKApp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Infrastructure;
+using Handin2_1;
+
+namespace ApplicationLogic
+{
+    public class PKApp
+    {
+        public void TheApp()
+        {
+            PersonKartotekDBUtil putil = new PersonKartotekDBUtil();
+
+            //****Zipliste****
+            //ZIPListe nyListe = new ZIPListe() { _Land = "Sydkorea", _By = "Seoul", _PostNummer = "998324", _ZIPListeID = 1 };
+            //putil.AddZIPListeDB(ref nyListe);
+            //putil.DeleteZipListeDB(ref nyListe);
+            //putil.UpdateZipListeDB(ref nyListe);
+            //Console.WriteLine("***Created ZipListe***");
+
+            //****ZIP****
+            //ZIP nyZip = new ZIP() { _Land = "Tyskland", _By = "Berlin", _PostNummer = "762648", _ZipID = 5, _ZIPListeID = 1 };
+            //putil.AddZipDB(ref nyZip);
+            //putil.UpdateZIPDB(ref nyZip);
+            //putil.DeleteZIPDB(ref nyZip);
+            //Console.WriteLine("***Delete Completed***");
+
+            //****Adresse ****
+            //Adresse nyAdresse = new Adresse()
+            //{ _AdresseType = "Privat", _GadeNavn = "Kalendervej", _GadeNummer = "82", _By = "Aarhus", _AdresseID = 1, _ZipID = 1 };
+            //putil.AddAdresseDB(ref nyAdresse);
+            //putil.DeleteAdresseDB(ref nyAdresse);
+            //putil.UpdateAdresse(ref nyAdresse);
+            //Console.WriteLine("***Deleted Adresse***");
+
+            //***Telefon***
+            Telefon nyTelefon = new Telefon() { _TeleSelskab = "OiSTER", _Nummer = "26851586", _TelefonType = "Privat", _TelefonID = 1, _PersonID = 1};
+            putil.AddTelefonDB(ref nyTelefon);
+            //putil.UpdateTelefonDB(ref nyTelefon);
+            //putil.DeleteTelefonDB(ref nyTelefon);
+            Console.WriteLine("***Added Telefon***");
+        }
+    }
+}
